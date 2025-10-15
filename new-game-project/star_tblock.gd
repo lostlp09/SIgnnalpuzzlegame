774cdfbc265bcb43f114011e3c7c889481dc137a
lostlp09 <mrnootnoot09@gmail.com>
2025-10-15 13:04:29 +0200

    you can move the objects now and you can connect them (still beta ) but i need to focus on how im going to run this

--- new-game-project/star_tblock.gd
+++ new-game-project/star_tblock.gd
@@ -1,21 +1,58 @@
 extends Sprite2D
+@onready var parent = $".."
 
-@onready var button1 = $Button
+@onready var button1 = null
+@onready var area = null
 var mousepressed = false
-
 func _ready() -> void:
+	button1 = self.get_child(0)
+	area = self.get_child(1)
 	button1.button_down.connect(pressed)
 	button1.button_up.connect(unpressed)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if mousepressed == true:
 		self.position = get_global_mouse_position()
-		
-
 func pressed()->void:
 	mousepressed = true
 func unpressed()->void:
+	if area.has_overlapping_areas():
+		var otherareas = area.get_overlapping_areas()
+		
+		var isoriginalareahigher = größerkleiner(area.get_parent(),otherareas[0].get_parent())
+		
+		if isoriginalareahigher ==true:
+			
+			if not self.is_in_group("Startblock"):
+				var gounderposition = otherareas[0].get_parent().position+  Vector2(0,146)
+				var allowed = true
+					
+				for i in parent.get_children():
+					if i.position == gounderposition:
+						allowed = false
+						break					
+				
+				if allowed :
+					self.position = gounderposition
+		else:
+			print("top")
+			var goupposition = otherareas[0].get_parent().position - Vector2(0,146)
+			if not  otherareas[0].get_parent().is_in_group("Startblock"):
+				var allowed = true
+				for i in parent.get_children():
+					if i.position == goupposition:
+						allowed = false
+						break		
+						pass
+				if allowed:
+					self.position = goupposition
+				
+						
+						
+	
 	mousepressed = false
+func größerkleiner(a,b)->bool:
+	print(a.position.y,b.position.y)
+	
+	return a.position.y > b.position.y  #true
+
 	
