68c2e940f455c7e0d0bfb052ac6c8df060525d4a
lostlp09 <mrnootnoot09@gmail.com>
2025-10-18 16:41:07 +0200

    added  loopblock
    you cant use it still but,you can already put it in the signal,eventhough it will do nothing

--- new-game-project/star_tblock.gd
+++ new-game-project/star_tblock.gd
@@ -17,68 +17,111 @@ func pressed()->void:
 	mousepressed = true
 func unpressed()->void:
 	if area.has_overlapping_areas():
-		var otherareas = area.get_overlapping_areas()
-		
-		var isoriginalareahigher = größerkleiner(area.get_parent(),otherareas[0].get_parent())
-		
-		if isoriginalareahigher ==true:
-			
-			if not self.is_in_group("Startblock"):
-				var gounderposition = otherareas[0].get_parent().position+  Vector2(0,78)
-				var allowed = true
-					
-				for i in parent.get_children():
-					if i.position == gounderposition:
-						allowed = false
-						break					
-				
-				if allowed :
-					var oldpos = self.get_meta("oldpos")
-					self.position = gounderposition
-					if self.get_parent() == allchildren and not oldpos == self.position: 
-						self.reparent(parent)
-					if(otherareas[0].get_parent() ==$"../StarTblock" or otherareas[0].get_parent().get_parent() ==allchildren) and not oldpos == self.position:
-						self.reparent(allchildren)		
-		else:
-			print("top")
-			var goupposition = otherareas[0].get_parent().position - Vector2(0,78)
-			if not  otherareas[0].get_parent().is_in_group("Startblock"):
-				var allowed = true
-				for i in parent.get_children():
-					if i.position == goupposition:
-						allowed = false
-						break		
-				if allowed:
-					var oldpos = self.get_meta("oldpos")
-					self.position = goupposition
-					if self.get_parent() == allchildren and  not oldpos == self.position: 
-						self.reparent(parent)
-					if (otherareas[0].get_parent() ==$"../StarTblock" or otherareas[0].get_parent().get_parent()==allchildren) and  not oldpos == self.position:
-						self.reparent(allchildren)	
-		var startpos = allchildren.get_parent().position
-		var steigen  = 0
-		for i in allchildren.get_children():
-			print(i)
-			steigen += 1
-			i.position = startpos
-			i.position.y += 78 * steigen
-
-
 	
+			var otherareas = area.get_overlapping_areas()
+			
+			var isoriginalareahigher = größerkleiner(area.get_parent(),otherareas[0].get_parent())
+			
+			if isoriginalareahigher ==true:
+				
+				if not self.is_in_group("Startblock"):
+					var gounderposition = 0
+					if  otherareas[0].get_parent().is_in_group("loop"):
+						print("under")
+						gounderposition = otherareas[0].get_parent().position+  Vector2(0,85)
+					elif area.get_parent().is_in_group("loop"):
+						gounderposition = otherareas[0].get_parent().position+  Vector2(0,112)
+					else:
+						gounderposition = otherareas[0].get_parent().position+  Vector2(0,78)
+						
+			
+					var allowed = true
+						
+					for i in parent.get_children():
+						if i.position == gounderposition:
+							allowed = false
+							break					
+					
+					if allowed :
+						var oldpos = self.get_meta("oldpos")
+						self.position = gounderposition
+						if self.get_parent() == allchildren and not oldpos == self.position: 
+							self.reparent(parent)
+						if(otherareas[0].get_parent() ==$"../StarTblock" or otherareas[0].get_parent().get_parent() ==allchildren) and not oldpos == self.position:
+							self.reparent(allchildren)		
+			else:
+				var goupposition =0
+				if  otherareas[0].get_parent().is_in_group("loop"):
+					print("top")
+					
+					goupposition = otherareas[0].get_parent().position - Vector2(0,112)
+				elif area.get_parent().is_in_group("loop"):
+					if otherareas[0].get_parent().is_in_group("Startblock"):
+						pass
+					else:
+						goupposition = otherareas[0].get_parent().position - Vector2(0,85)
+					
+				else:	
+					goupposition = otherareas[0].get_parent().position - Vector2(0,78)
+				if not  otherareas[0].get_parent().is_in_group("Startblock"):
+					var allowed = true
+					for i in parent.get_children():
+						if i.position == goupposition:
+							allowed = false
+							break		
+					if allowed:
+						var oldpos = self.get_meta("oldpos")
+						self.position = goupposition
+						if self.get_parent() == allchildren and  not oldpos == self.position: 
+							self.reparent(parent)
+						if (otherareas[0].get_parent() ==$"../StarTblock" or otherareas[0].get_parent().get_parent()==allchildren) and  not oldpos == self.position:
+							self.reparent(allchildren)	
+			var startpos = allchildren.get_parent().position
+			var steigen  = 0
+			var last_object = null
+			for i in allchildren.get_children():
+				if last_object == null:
+					if i.is_in_group("loop"):
+						i.position= startpos + Vector2(0,112)
+					else:
+						i.position =startpos + Vector2(0,78)
+					last_object = i
+					
+				else:
+					if i.is_in_group("loop"):
+						i.position = Vector2(startpos.x ,last_object.position.y + 112)
+						
+					else:
+						if last_object.is_in_group("loop"):
+							i.position = Vector2(startpos.x ,last_object.position.y + 85)
+						else:
+							i.position = Vector2(startpos.x ,last_object.position.y + 78)
+
+					last_object = i
 	else:
-		
 		if self.get_parent() == allchildren:
 			var startpos = allchildren.get_parent().position
 			self.reparent(parent)
-			var steigen  = 0
+			var last_object = null
 			for i in allchildren.get_children():
-				print(i)
-				steigen += 1
-				i.position = startpos
-				i.position.y += 60 * steigen
-				
+				if last_object == null:
+					if i.is_in_group("loop"):
+						i.position= startpos + Vector2(0,112)
+					else:
+						i.position =startpos + Vector2(0,78)
+					last_object = i
+				else:
+					if i.is_in_group("loop"):
+						i.position = Vector2(startpos.x ,last_object.position.y + 112)
+						
+					else:
+						if last_object.is_in_group("loop"):
+							i.position = Vector2(startpos.x ,last_object.position.y + 85)
+						else:
+							i.position = Vector2(startpos.x ,last_object.position.y + 78)
+
+					last_object = i
 		
-							
 	self.set_meta("oldpos",self.position)
 	mousepressed = false
 func größerkleiner(a,b)->bool:
